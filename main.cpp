3cc0de52328849f0060632ae27a86a7f9d356dd2
pjoter <koleczkowsky@gmail.com>
2019-04-30 19:13:53 +0200

    Projekt od przebudowania, mutex do poprawy!

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <memory>
 #include <condition_variable>
 #include <ncurses.h>
+#include <exception> 
 
 thread_local std::mt19937 gen{ std::random_device{}() };
 
@@ -25,12 +26,12 @@ var random(var min, var max) {
 int rows = 0;
 int columns = 0;
 bool runningLoop = true;
-std::mutex mtx1;
-std::mutex mtx2;
-std::mutex mtx3;
-std::condition_variable cv1;
-std::condition_variable cv2;
-bool isBallHit = false;
+std::mutex mtx;
+std::condition_variable cv;
+int indexOfSecondBall;
+int resultantVerticalShift;
+int resultantHorizontalShift;
+int resultantVelocity;
 
 class Ball
 {
@@ -42,11 +43,13 @@ class Ball
 	int xSpeed;
 	int ySpeed;
 	int velocity;
+	bool exitThread;
 
 public:
 
 	Ball(int xPosition, int yPosition)
 	{
+		exitThread = false;
 		horizontalShift = 1;
 		verticalShift = 1;
 		this->xPosition = xPosition;
@@ -109,6 +112,16 @@ public:
 		return verticalShift;
 	}
 
+	int GetVelocity()
+	{
+		return velocity;
+	}
+
+	void SetVelocity(int velocity)
+	{
+		this->velocity = velocity;
+	}
+
 	void SetHorizontalShift(int horizontalShift)
 	{
 		this->horizontalShift = horizontalShift;
@@ -117,6 +130,16 @@ public:
 	void SetVerticalShift(int verticalShift)
 	{
 		this->verticalShift = verticalShift;
+	}
+
+	void StopTheBall()
+	{
+		this->exitThread = false;
+	}
+
+	bool CheckIfBallStopped()
+	{
+		return exitThread;
 	}
 
 	void MoveRight()
@@ -167,30 +190,27 @@ public:
 		verticalShift = 1;
 	}
 
-	void DisplaceBall()
+	void DisplaceBall(double x)
 	{
-		xPosition += horizontalShift;
-		yPosition += verticalShift;
-	}
-
-	void SetBallVelocity(int velocity)
-	{
-		this->velocity = velocity;
+		xPosition += (int)(x*horizontalShift);
+		yPosition += (int)(x*verticalShift);
 	}
 
 	void BallCollisionWithWall()
 	{
 		// lustrzane odbicia kulek
-		if(GetXPosition() == 0 || GetXPosition() == rows - 1) horizontalShift = -horizontalShift;
-		if(GetYPosition() == 0 || GetYPosition() == columns - 1) verticalShift = -verticalShift;
-		DisplaceBall();
+		if(GetXPosition() <= 0 || GetXPosition() >= rows - 1) horizontalShift = -horizontalShift;
+		if(GetYPosition() <= 0 || GetYPosition() >= columns - 1) verticalShift = -verticalShift;
+		if(velocity == 50) DisplaceBall(1.0);
+		//else if(velocity == 150) DisplaceBall(2.0);
+		else DisplaceBall(1.0);
 	}
 };
 
 std::vector<Ball*> balls;
 std::vector<std::thread> threadsOfBalls;
 
-bool BallInMutex(int nrOfBall)
+bool DidBallsHit(int nrOfBall)
 {
 	int xCoord = balls[nrOfBall]->GetXPosition();
 	int yCoord = balls[nrOfBall]->GetYPosition();
@@ -207,6 +227,7 @@ bool BallInMutex(int nrOfBall)
 
 int GetIndexOfSecondBall(int nrOfBall)
 {
+	int n;
 	int xCoord = balls[nrOfBall]->GetXPosition();
 	int yCoord = balls[nrOfBall]->GetYPosition();
 
@@ -214,80 +235,103 @@ int GetIndexOfSecondBall(int nrOfBall)
 	{
 		if((balls[i]->GetXPosition() - xCoord == 0) && (balls[i]->GetYPosition() - yCoord == 0) && i != nrOfBall)
 		{
+			n = i;
 			return i;
 		}
 	}
-	return -1;
+	return n;
 }
 
 int GetResultantHorizontalShift(int i, int j)
 {
-	int firstHorizontalShift = balls[i]->GetHorizontalShift();
-	int secondHorizontalShift = balls[j]->GetHorizontalShift();
+	int horizontalShiftOfFirstBall = balls[i]->GetHorizontalShift();
+	int horizontalShiftOfSecondBall = balls[j]->GetHorizontalShift();
 	
-	if(firstHorizontalShift == secondHorizontalShift) return firstHorizontalShift;
-	if(firstHorizontalShift > secondHorizontalShift) return firstHorizontalShift;
-	else return secondHorizontalShift;
+	if(horizontalShiftOfFirstBall == horizontalShiftOfSecondBall) return horizontalShiftOfFirstBall;
+	else if((horizontalShiftOfFirstBall == 0 && horizontalShiftOfSecondBall == 1) || (horizontalShiftOfFirstBall == 1 && horizontalShiftOfSecondBall == 0))
+	{
+		return 1;
+	}
+	else if((horizontalShiftOfFirstBall == 0 && horizontalShiftOfSecondBall == -1) || (horizontalShiftOfFirstBall == -1 && horizontalShiftOfSecondBall == 0))
+	{
+		return -1;
+	}
+	else return 0;
 }
 
 int GetResultantVerticalShift(int i, int j)
 {
-	int firstVerticalShift = balls[i]->GetVerticalShift();
-	int secondVerticalShift = balls[j]->GetVerticalShift();
+	int verticalShiftOfFirstBall = balls[i]->GetVerticalShift();
+	int verticalShiftOfSecondBall = balls[j]->GetVerticalShift();
 
-	if(firstVerticalShift == secondVerticalShift) return firstVerticalShift;
-	if(firstVerticalShift > secondVerticalShift) return firstVerticalShift;
-	else return secondVerticalShift;
+	if(verticalShiftOfFirstBall == verticalShiftOfSecondBall) return verticalShiftOfFirstBall;
+	else if((verticalShiftOfFirstBall == 0 && verticalShiftOfSecondBall == 1) || (verticalShiftOfFirstBall == 1 && verticalShiftOfSecondBall == 0))
+	{
+		return 1;
+	}
+	if((verticalShiftOfFirstBall == 0 && verticalShiftOfSecondBall == -1) || (verticalShiftOfFirstBall == -1 && verticalShiftOfSecondBall == 0))
+	{
+		return -1;
+	}
+	else return 0;
+}
+
+int GetResultantVelocity(int i, int j)
+{
+	int horizontalShiftOfFirstBall = balls[i]->GetHorizontalShift();
+	int horizontalShiftOfSecondBall = balls[j]->GetHorizontalShift();
+	int verticalShiftOfFirstBall = balls[i]->GetVerticalShift();
+	int verticalShiftOfSecondBall = balls[j]->GetVerticalShift();
+
+	if(horizontalShiftOfFirstBall == horizontalShiftOfSecondBall && verticalShiftOfFirstBall == verticalShiftOfSecondBall) return 50;
+	else if(horizontalShiftOfFirstBall != horizontalShiftOfSecondBall && verticalShiftOfFirstBall == verticalShiftOfSecondBall) return 150;
+	else if(horizontalShiftOfFirstBall == horizontalShiftOfSecondBall && verticalShiftOfFirstBall != verticalShiftOfSecondBall) return 50;
+	else return 150;
 }
 
 void BallThreadFunction(int nrOfBall)
 {
-	bool ballLeavingMutex = false;
-	int indexOfSecondBall;
-	int resultantVerticalShift;
-	int resultantHorizontalShift;
+	int velocity;
 
 	while(runningLoop)
 	{
 		balls[nrOfBall]->BallCollisionWithWall();
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+		std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-		if(BallInMutex(nrOfBall))
+		std::unique_lock<std::mutex> lock(mtx);
+
+		if(DidBallsHit(nrOfBall))
 		{
-			std::unique_lock<std::mutex> lock2(mtx2);
 			indexOfSecondBall = GetIndexOfSecondBall(nrOfBall);
 
 			resultantVerticalShift = GetResultantVerticalShift(nrOfBall, indexOfSecondBall);
 			resultantHorizontalShift = GetResultantHorizontalShift(nrOfBall, indexOfSecondBall);
-			balls[indexOfSecondBall]->SetVerticalShift(resultantVerticalShift);
-			balls[indexOfSecondBall]->SetHorizontalShift(resultantHorizontalShift);
+			velocity = GetResultantVelocity(nrOfBall, indexOfSecondBall);
+			balls[nrOfBall]->SetVelocity(velocity);
+			balls[nrOfBall]->SetVerticalShift(resultantVerticalShift);
+			balls[nrOfBall]->SetHorizontalShift(resultantHorizontalShift);
+			balls[indexOfSecondBall]->StopTheBall();
 
-			balls[nrOfBall]->SetXPosition(1000);
-			balls[nrOfBall]->SetYPosition(1000);
+			balls[indexOfSecondBall]->SetXPosition(1000);
+			balls[indexOfSecondBall]->SetYPosition(1000);
 
-			isBallHit = true;
-			while(runningLoop)
-			{
-				cv2.wait(lock2);
-			}
-			mtx2.unlock();
-
-			std::unique_lock<std::mutex> lock1(mtx1);
-
-			while(!isBallHit && runningLoop)
-			{
-				cv1.wait(lock1);
-			}
-			isBallHit = false;
-			lock1.unlock();
+			/*cv.wait(lock);*/
+			
+			//std::terminate();
+			threadsOfBalls[indexOfSecondBall].join();
+			lock.unlock();
 		}
+
+		
+
+		// zakończenie wątku, nie wait
 	}
 }
 
 void CreateBall()
 {
 	int i = 0;
-	while(i < 10)
+	while(i < 15)
 	{
 		getmaxyx(stdscr, rows, columns);
 		balls.push_back(new Ball(rows / 2, columns / 2));
@@ -301,7 +345,8 @@ void TerminateThreadsOfBalls()
 {
 	for (int i = 0; i < threadsOfBalls.size(); ++i)
 	{
-		threadsOfBalls[i].join();
+		if(balls[i]->CheckIfBallStopped() == false) continue;
+		else threadsOfBalls[i].join();
 	}
 }
 
@@ -315,8 +360,7 @@ void PressKeyToEnd()
 		char key = getch();
 		if (key == 'q') runningLoop = false;
 		else std::this_thread::sleep_for(std::chrono::milliseconds(20));
-		cv1.notify_all();
-		cv2.notify_all();
+		cv.notify_all();
 	}
 }
 
